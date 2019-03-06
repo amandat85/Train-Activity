d6b71e1bd1728745b5471d6f44fb259699910a31
amandat <amandatome@me.com>
2019-03-06 09:56:22 -0500

    Functioning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,118 @@
-//INITIALIZE FIREBASE================================
-// var config = {
-//     apiKey: "AIzaSyC8iV50tAKbwEqhyt1W2eIUIpcVLrCjMzA",
-//     authDomain: "trainscheduler-3734a.firebaseapp.com",
-//     databaseURL: "https://trainscheduler-3734a.firebaseio.com",
-//     projectId: "trainscheduler-3734a",
-//     storageBucket: "trainscheduler-3734a.appspot.com",
-//     messagingSenderId: "1088544218827"
-//   };
-//   firebase.initializeApp(config);
-//VARIABLES=========================================
-// var database = firebase.database();
-var train = "";
-console.log(train);
-var destination = "";
-var startTime = "";
-var frequency = 0;
-//ON CLICK INPUT SUBMIT==============================
-document.querySelector("#addTrain").addEventListener("click", function(event){
-  event.preventDefault();
-  console.log(event);
-  train = document.querySelector("#inputTrain").value.trim();
-  destination = document.querySelector("#destination").value.trim();
-  startTime = document.querySelector("#firstTrain").value.trim();
-  frequency = document.querySelector("#frequency").value.trim();
-  console.log(train);
-  console.log(destination);
-  console.log(startTime);
-  console.log(frequency);
+$(document).ready(function () {
+
+  function time() {
+    setInterval(function () {
+      var currentTime = moment();
+      document.querySelector("#currentTime").innerHTML = moment(currentTime).format("HH:mm.ss");
+    }, 1000);
+  }
+  time();
+
+  //INITIALIZE FIREBASE==========================================================================================================
+  var config = {
+    apiKey: "AIzaSyC8iV50tAKbwEqhyt1W2eIUIpcVLrCjMzA",
+    authDomain: "trainscheduler-3734a.firebaseapp.com",
+    databaseURL: "https://trainscheduler-3734a.firebaseio.com",
+    projectId: "trainscheduler-3734a",
+    storageBucket: "trainscheduler-3734a.appspot.com",
+    messagingSenderId: "1088544218827"
+  };
+  firebase.initializeApp(config);
+  //VARIABLES=====================================================================================================================
+  var database = firebase.database();
+
+  //ON CLICK INPUT SUBMIT=========================================================================================================
+  document.querySelector("#addTrain").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    //EMPTY FIELDS ON SUBMIT
+    if ($("#inputTrain").val().trim() === "" || $("#destination").val().trim() === "" || $("#firstTrain").val().trim() === "" || $("#frequency").val().trim() === "") {
+    }
+    else {
+      //VARIABLES====================================================
+      var trainAdded = document.querySelector("#inputTrain").value.trim();
+      var destinationLocation = document.querySelector("#destination").value.trim();
+      var startTimeMilitary = moment(document.querySelector("#firstTrain").value.trim(), "HH.mm").format();
+      var frequencyMin = parseInt(document.querySelector("#frequency").value.trim());
+      //CREATE TEMPORARY OBJECT FOR TRAIN DATA==============================================
+      var newTrain = {
+        train: trainAdded,
+        destination: destinationLocation,
+        startTime: startTimeMilitary,
+        frequency: frequencyMin,
+      };
+      //PUSH TO DATABASE=================================================
+      database.ref("/newTrain").push(newTrain);
+    }
+    //CLEAR FORM==========================================================
+    document.getElementById("inputTrain").value = "";
+    document.getElementById("destination").value = "";
+    document.getElementById("firstTrain").value = "";
+    document.getElementById("frequency").value = "";
+  });
+
+  database.ref("/newTrain").on("child_added", function(childSnapshot){
+   
+    var trainAdded = childSnapshot.val().train;
+    var destinationLocation = childSnapshot.val().destination;
+    var startTimeConverted = moment(childSnapshot.val().startTime, "HH:mm").subtract(1, "years");
+    var frequencyMin = childSnapshot.val().frequency;
+
+  //CALCULATE NEXT TRAIN TIME======================================
+    var timeMin = moment().diff(moment(startTimeConverted), "minutes");
+
+    var timeRemaining = timeMin % frequencyMin;
+
+    var minToArrival = frequencyMin - timeRemaining;
+
+    var nextTrain = moment().add(minToArrival, "minutes");
+
+  //ADD NEW TRAIN TO TABLE========================================
+  var newRow = document.createElement("tbody");
+  newRow.setAttribute("class", "newInfo");
+  
+  var columnTrainName = document.createElement("td");
+  columnTrainName.innerHTML = trainAdded;
+  newRow.appendChild(columnTrainName);
+
+  var columnDestination = document.createElement("td");
+  columnDestination.innerHTML = destinationLocation;
+ newRow.appendChild(columnDestination);
+
+  var columnFrequency = document.createElement("td");
+  columnFrequency.innerHTML = frequencyMin;
+  newRow.appendChild(columnFrequency);
+
+  var columnNextTrain = document.createElement("td");
+  columnNextTrain.innerHTML = moment(nextTrain).format("LT");
+  newRow.appendChild(columnNextTrain);
+
+  var columnArrival = document.createElement("td");
+  columnArrival.innerHTML = minToArrival;
+  newRow.appendChild(columnArrival);
+
+  document.querySelector("#trains").appendChild(newRow);
+
+});
 
 
-})
+// setInterval(function(){
+//   var counter = 10
+//   counter--;
 
-//On click event to get train input
-//Use val().trim() to capture the input
-//Create temporary object to hold train data - fields should match the variables from the input field
-//Push this information to Firebase database
-//Clear all the text boxes on submit
-//Create the Firebase event to add the new train information. Take snapshot of the event -"child_added?"
-//Format the time with moment.js to be military time
-//Calculate (using moment.js function and math), the next train minutes using the frequency and first train value
-//Push to database
-//Create new rows and append input information
-//Add user ability to remove row
-//Reset page every 60 secs
-//Set current time. Tell it to update every 60 secs
+// }, 10000);
+// function reloadRow() {
+//   setInterval(function () {
+//     document.querySelector(".newInfo")
+//   }, 1000);
+// }
+// reloadRow();
+  
+  //Fix form validation
+  //Add user ability to remove row
+  //Reset page every 60 secs
+  //error handler
+  //add local storage
 
+
+});
